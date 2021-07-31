7b827423686723883863028e65051a25a379787c
Mika Rafaralahy <mrafa099@uottawa.ca>
2021-07-31 12:00:55 -0500

    1.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,11 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+    <header> <nav>
+      <a href="index.js">Home</a>
+      <a href="page-2.js"> Page 2</a>
+      <a href="using-typescript.tsx">Using typescript</a>
+    </nav> </header>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
